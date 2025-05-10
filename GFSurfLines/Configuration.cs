1fb31a41fae58c7ac71295a4f4bb398911e4a62b
Rrathmal <rrathmal@gmail.com>
2025-05-09 23:20:34 -0400

    MapId update
    Updated with all moon mapIds

--- GFSurfLines/Configuration.cs
+++ GFSurfLines/Configuration.cs
@@ -11,7 +11,7 @@ public class Configuration : IPluginConfiguration
 
     public int Volume { get; set; } = 50;
     public bool Enabled { get; set; } = true;
-    public List<uint> ValidMaps { get; set; } = new List<uint>() { 1011 };
+    public List<uint> ValidMaps { get; set; } = new List<uint>() { 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025, 1026 };
 
     // the below exist just to make saving less cumbersome
     public void Save()
